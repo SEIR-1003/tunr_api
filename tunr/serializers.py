43d38ae1b12a36521258183919a37a3d34414f25
jataubman <jeremy.taubman@generalassemb.ly>
2022-12-20 14:38:48 -0500

    serializers in order

--- tunr/serializers.py
+++ tunr/serializers.py
@@ -1,16 +1,6 @@
 from rest_framework import serializers
 from .models import Artist, Song
 
-class ArtistSerializer(serializers.HyperlinkedModelSerializer):
-    songs = serializers.HyperlinkedRelatedField(
-        view_name='song_detail',
-        many=True,
-        read_only=True
-    )
-
-    class Meta:
-        model = Artist
-        fields =('id', 'photo_url', 'nationality', 'name', 'songs')
 
 
 class SongSerializer(serializers.HyperlinkedModelSerializer):
@@ -21,3 +11,16 @@ class SongSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Song
         fields = ('id', 'artist', 'title', 'album', 'preview_url',)
+
+
+class ArtistSerializer(serializers.HyperlinkedModelSerializer):
+    songs = SongSerializer(
+        many=True,
+        read_only=True
+    )
+
+    class Meta:
+        model = Artist
+        fields =('id', 'photo_url', 'nationality', 'name', 'songs')
+
+
